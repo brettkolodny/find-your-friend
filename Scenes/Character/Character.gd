ac5afffa80bffe751331b5fa97b03bd00a8ace03
brettkolodny <brettkolodny@gmail.com>
2022-01-08 16:24:32 -0500

    Spawn peeps

---
+++ Scenes/Character/Character.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+const SCALE_AMOUNT = 0.1
+
+var is_growing = false
+
+export var scale_factor = 1
+
+onready var initial_scale = self.scale.y
+
+func _ready():
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+
+	var initial_scale_state = rng.randf_range(0, SCALE_AMOUNT)
+	
+	if randi() % 2:
+		self.is_growing = true
+		self.scale = Vector2(self.scale.x, self.scale.y + initial_scale_state)
+	else :
+		self.scale = Vector2(self.scale.x, self.scale.y - initial_scale_state)
+
+func _process(delta):
+	var new_scale = self.scale.y
+	
+	if is_growing:
+		new_scale +=  scale_factor * (delta / 2)
+		
+		if new_scale > initial_scale + SCALE_AMOUNT:
+			self.is_growing = false
+			return
+		
+	else:
+		new_scale -= scale_factor * (delta / 2)
+		
+		if new_scale < initial_scale - SCALE_AMOUNT:
+			self.is_growing = true
+			return
+		
+	self.scale = Vector2(self.scale.x, new_scale)
--- Scenes/Level/Level.gd
+++ Scenes/Level/Level.gd
@@ -1,17 +1,34 @@
 extends Node
 
 const PEEP_NODE = preload("res://Scenes/Peep/Peep.tscn")
-export var num_peeps = 10
+const PLAY_AREA_PADDING = Vector2(100, 100)
+
+export var num_peeps = 50
+export var character_scale = Vector2(1.5, 1.5)
+
 
 func _ready():
-	var peep: Node2D = PEEP_NODE.instance()
-	peep.scale = Vector2(1, 1)
-	peep.position = Vector2(500, 200)
+	var play_area_position = $ReferenceRect/PlayArea.rect_position
+	var play_area_size = $ReferenceRect/PlayArea.rect_size
 	
-	var peep2: Node2D = PEEP_NODE.instance()
-	peep2.scale = Vector2(2, 2)
-	peep2.position = Vector2(500, 300)
+	var spawn_area_x = (play_area_size.x - PLAY_AREA_PADDING.x)
+	var spawn_area_y = (play_area_size.y - PLAY_AREA_PADDING.y)
 	
-	self.add_child(peep)
-	self.add_child(peep2)
+	var punch_position_x = play_area_position.x + (PLAY_AREA_PADDING.x / 2)
+	var punch_position_y = play_area_position.y + (PLAY_AREA_PADDING.y / 2)
+	
+	for _i in range(num_peeps):
+		var new_peep: Node2D = PEEP_NODE.instance()
+		new_peep.scale = character_scale
+		
+		var rand_x = randi() % int(spawn_area_x)
+		var rand_y = randi() % int(spawn_area_y)
+		
+		var new_peep_position = Vector2(rand_x + punch_position_x, rand_y + punch_position_y)
+		print(new_peep_position)
+		new_peep.global_position = new_peep_position
+		
+		self.add_child(new_peep)
+	
+	
 	Global.randomize_peeps()
