6aa3b2966e2bd29468a236425e4a204f88dc355c
brettkolodny <brettkolodny@gmail.com>
2022-01-09 23:55:22 -0500

    Add countdown timer

--- Scenes/Tutorial/Tutorial.gd
+++ Scenes/Tutorial/Tutorial.gd
@@ -1,21 +1,5 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_FriendPreviewTutorial_mouse_entered():
 	$PreviewCardTutorial.visible = false
 	$ZoomTutorial.visible = true
