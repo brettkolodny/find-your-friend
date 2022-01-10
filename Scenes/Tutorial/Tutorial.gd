312881e9f29fb8423d8d5a5f942c4e1fda4df8d6
brettkolodny <brettkolodny@gmail.com>
2022-01-09 20:02:54 -0500

    Fix tutorial

--- Scenes/Tutorial/Tutorial.gd
+++ Scenes/Tutorial/Tutorial.gd
@@ -19,7 +19,7 @@ func _ready():
 func _on_FriendPreviewTutorial_mouse_entered():
 	$PreviewCardTutorial.visible = false
 	$ZoomTutorial.visible = true
-	$Level/FriendPreviewTutorial/CollisionShape2D.disabled
+	$Level/FriendPreviewTutorial/CollisionShape2D.disabled = true
 
 
 func _process(delta):
@@ -28,4 +28,4 @@ func _process(delta):
 		$Level/FindFriendTutorial/FindFriendTutorialInstructions.visible = true
 	elif Input.is_action_just_released("ui_right_click"):
 		$ZoomTutorial.visible = true
-		$Level/FindFriendTutorial/TextureRect.visible = false
+		$Level/FindFriendTutorial/FindFriendTutorialInstructions.visible = false
